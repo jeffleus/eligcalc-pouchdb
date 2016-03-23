3d29201bcf57d67c482b6087199faba89bdd0083
Jeff Leininger <jeffleus@gmail.com>
2016-03-22 22:21:03 -0700

    adding calendars feature module
    added a module, service, controller and view and linked source files in
    the index file

---
+++ www/app/calendars/calendar.service.js
@@ -0,0 +1,15 @@
+// dataservice.js
+(function() {
+    'use strict';
+
+    angular
+        .module('eligcalc.calendar')
+        .service('CalendarSvc', calendarService);
+
+	calendarService.$inject = [];
+	
+    function calendarService() {
+        /*jshint validthis: true*/
+        var self = this;
+	}
+})();
